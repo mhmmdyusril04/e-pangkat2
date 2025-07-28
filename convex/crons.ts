c12bdbcf706f8f404e8725dc9d2ca946739e5518
yusrilfauzi04 <227006067@student.unsil.ac.id>
2025-07-28 09:00:42 +0700

    test_reminder_otomatis

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,13 +1,13 @@
-import { cronJobs } from 'convex/server';
-import { internal } from './_generated/api';
+import { cronJobs } from "convex/server";
+import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
 // if (process.env.NODE_ENV === 'production') {
 crons.daily(
-    'check_promotion_reminders_daily',
-    { hourUTC: 17, minuteUTC: 5 },
-    internal.reminder.checkAndSendPromotionReminders
+  "check_promotion_reminders_daily",
+  { hourUTC: 2, minuteUTC: 2 }, // Jam 09:00 WIB
+  internal.reminder.checkAndSendPromotionReminders
 );
 // } else {
 // crons.interval('check_promotion_reminders_for_dev', { minutes: 1 }, internal.reminder.checkAndSendPromotionReminders);
